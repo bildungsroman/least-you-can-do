8e38332e6b380a40c8e2c0b3490cd5aebb453f77
Anna Spysz <anna.spysz@stackery.io>
2019-10-21 19:29:56 -0700

    Configure S3

--- src/handleSubmit/index.js
+++ src/handleSubmit/index.js
@@ -1,5 +1,4 @@
 const qs = require('qs');
-const fs = require('fs');
 const AWS = require('aws-sdk');
 
 exports.handler = async message => {
@@ -7,10 +6,8 @@ exports.handler = async message => {
   const formData = qs.parse(message.body);
   console.log(formData);
 
-  const responseBody = fs.readFileSync('./submitted.html', 'utf8');
-
   return {
     statusCode: 302,
-    headers: {'Location': responseBody}
+    headers: {'Location': 'https://stackery.io'}
   };
 };
