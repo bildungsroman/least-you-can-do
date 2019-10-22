eaf2a7e17933271c9deb1c54e812f1c8ad182ca7
Anna Spysz <anna.spysz@stackery.io>
2019-10-21 19:38:13 -0700

    Refactor for S3

--- src/getForm/index.js
+++ src/getForm/index.js
@@ -1,8 +1,32 @@
 const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const mime = require('mime-types');
+const recursiveReaddir = require('recursive-readdir');
+const s3 = new AWS.S3();
 
 exports.handler = async event => {
   // Log http request
   console.log(event);
+  try {
+    await Promise.all([
+      uploadStaticContent()
+    ]);
+
+    // Send success signal back to CloudFormation
+    await cfnCR.sendSuccess('PopulateFrontendContent', {}, message);
+
+    console.log('Succeeded in uploading site content!')
+  } catch (err) {
+    console.error('Failed to upload site content:');
+    console.error(err);
+
+    // Send error message back to CloudFormation
+    await cfnCR.sendFailure(err.message, message);
+
+    // Re-throw error to ensure invocation is marked as a failure
+    throw err;
+  }
 
   const responseBody = fs.readFileSync('./form.html', 'utf8');
   // Build an HTTP response.
@@ -16,3 +40,20 @@ exports.handler = async event => {
 
   return response;
 };
+
+// Upload site content from 'static' directory
+async function uploadStaticContent() {
+  // List files in 'static' directory
+  const files = await recursiveReaddir('static');
+
+  // Upload files asynchronously to frontend content object store
+  const promises = files.map(file => s3.putObject({
+    Bucket: process.env.BUCKET_NAME,
+    Key: path.relative('static', file),
+    Body: fs.createReadStream(file),
+    ContentType: mime.lookup(file) || 'application/octet-stream',
+    ACL: 'public-read'
+  }).promise());
+
+  await Promise.all(promises);
+}
